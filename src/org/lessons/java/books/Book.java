49e6684f725b17f30ff9e2567a9fc9f48b0d24e2
Andreaserra1997 <serra.andrea1997@gmail.com>
2023-10-24 17:10:28 +0200

    print books

--- src/org/lessons/java/books/Book.java
+++ src/org/lessons/java/books/Book.java
@@ -1,4 +1,63 @@
 package org.lessons.java.books;
 
 public class Book {
+    private String title;
+    private int numPages;
+    private String author;
+    private String publisher;
+
+    public Book(String title, int numPages, String author, String publisher) {
+        setTitle(title);
+        setNumPages(numPages);
+        setAuthor(author);
+        setPublisher(publisher);
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        if (title != null && !title.isEmpty()) {
+            this.title = title;
+        } else {
+            throw new IllegalArgumentException("Il titolo del libro non può essere vuoto.");
+        }
+    }
+
+    public int getNumPages() {
+        return numPages;
+    }
+
+    public void setNumPages(int numPages) {
+        if (numPages > 0) {
+            this.numPages = numPages;
+        } else {
+            throw new IllegalArgumentException("Il numero di pagine deve essere maggiore di zero.");
+        }
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        if (author != null && !author.isEmpty()) {
+            this.author = author;
+        } else {
+            throw new IllegalArgumentException("Il nome dell'autore non può essere vuoto.");
+        }
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        if (publisher != null && !publisher.isEmpty()) {
+            this.publisher = publisher;
+        } else {
+            throw new IllegalArgumentException("Il nome dell'editore non può essere vuoto.");
+        }
+    }
 }
--- src/org/lessons/java/books/Main.java
+++ src/org/lessons/java/books/Main.java
@@ -1,4 +1,47 @@
 package org.lessons.java.books;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Inserisci il numero di libri nel catalogo: ");
+        int n = scanner.nextInt();
+        scanner.nextLine();
+
+        Book[] catalog = new Book[n];
+
+        for (int i = 0; i < n; i++) {
+            try {
+                System.out.println("Inserisci i dati del libro #" + (i + 1) + ":");
+                System.out.print("Titolo: ");
+                String title = scanner.nextLine();
+                System.out.print("Numero di pagine: ");
+                int numPages = scanner.nextInt();
+                scanner.nextLine();
+                System.out.print("Autore: ");
+                String author = scanner.nextLine();
+                System.out.print("Editore: ");
+                String publisher = scanner.nextLine();
+
+                catalog[i] = new Book(title, numPages, author, publisher);
+                System.out.println("Libro aggiunto al catalogo.");
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+                i--;
+            }
+        }
+
+        System.out.println("Libri:");
+        for (Book book : catalog) {
+            System.out.println("Titolo: " + book.getTitle());
+            System.out.println("Pagine: " + book.getNumPages());
+            System.out.println("Autore: " + book.getAuthor());
+            System.out.println("Editore: " + book.getPublisher());
+            System.out.println();
+        }
+        scanner.close();
+    }
 }
